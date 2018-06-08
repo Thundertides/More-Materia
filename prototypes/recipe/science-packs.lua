1d555cb042ac0f21e43fa5b9c3c6bc8259447c90
Thundertides <40047664+Thundertides@users.noreply.github.com>
2018-06-08 14:43:33 -0600

    Update and rename science-packs to science-packs.lua

---
+++ prototypes/recipe/science-packs.lua
@@ -0,0 +1,31 @@
+data:extend({
+ {
+    type = "recipe",
+    name = "military-science-pack",
+    enabled = "false",
+    energy-required = 12,
+    ingredients = 
+    {
+      {"advanced-circuit",1},
+      {"electric-mining-drill",1},
+      {"beaker",1},
+
+    },
+    result = "science-pack-3"
+  },
+   {
+    type = "recipe",
+    name = "science-pack-4",
+    enabled = "false",
+    energy-required = 12,
+    result-count = 2
+    ingredients = 
+    {
+      {"grenade",1},
+      {"gun-turret",1},
+      {"beaker",1},
+
+    },
+    result = "military-science-pack"
+  },
+})
