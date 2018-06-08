da25a8ecd727164ce9c874e752f015971e89f195
Thundertides <40047664+Thundertides@users.noreply.github.com>
2018-06-08 16:41:01 -0600

    Update and rename geode-refiner to geode-refiner.lua

---
+++ prototypes/item/geode-refiner.lua
@@ -0,0 +1,15 @@
+
+  data:extend{
+{
+    type = "item",
+    name = "geode-refiner",
+    icon = "__base__/graphics/icons/assembling-machine-2.png",
+    icon_size = 32,
+    flags = {"goes-to-quickbar"},
+    subgroup = "production-machine",
+    order = "b[geode-refiner]",
+    place_result = "geode-refiner",
+    stack_size = 50
+  },
+  }
+
