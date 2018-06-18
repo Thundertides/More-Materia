4a3a572bf8fe2577c8d0b39c86bddc82d796a687
Thundertides <40047664+Thundertides@users.noreply.github.com>
2018-06-17 21:46:00 -0600

    Update gems.lua

--- prototypes/recipe/gems.lua
+++ prototypes/recipe/gems.lua
@@ -1,15 +1,75 @@
 data:extend({
- {
+{
     type = "recipe",
-    name = "diamond",
+    name = "basic-gem-processing",
     enabled = "false",
-    energy_required = 10
-    category = "gem-refining"
+    energy_required = 5,
+    icon = "__More-Materia__/graphics/geodude.png",
+    icon_size = 128,
+    subgroup = "raw-material",
+    category = "gem-refining",
     ingredients = 
     {
-      {"geodude",10},
-      {"coal",10},
+      {"geodude",1},
     },
-    result = "diamond"
+    results =
+    { 
+    {
+        name = "diamond",
+        probability = 0.08,
+        amount = 1
+      },
+        {
+        name = "emerald",
+        probability = 0.15,
+        amount = 1
+      },
+      {
+        name = "geodude",
+        probability = 0.65,
+        amount = 1
+      },
+      {
+        name = "ruby",
+        probability = 0.12,
+        amount = 1
+      }
+      }
   },
-})
+   {
+    type = "recipe",
+    name = "ruby-board",
+    enabled = "false",
+    ingredients = 
+    {
+      {"ruby",2},
+      
+      
+      
+      
+      
+      
+      
+    },
+    result = "ruby-board"
+  },
+     {
+    type = "recipe",
+    name = "emerald-board",
+    enabled = "false",
+    ingredients = 
+    {
+      {"emerald",2},
+      
+      
+      
+      
+      
+      
+      
+    },
+    result = "emerald-board"
+  },
+  }
+  
+)
