417d44b799048e7581aa18aaedd0bec734a1bb49
Thundertides <40047664+Thundertides@users.noreply.github.com>
2023-04-06 21:25:52 -0600

    Create solar-panel.lua

---
+++ prototypes/recipe/solar-panel.lua
@@ -0,0 +1,36 @@
+data:extend({
+ {
+    type = "recipe",
+    name = "solar-panel",
+    enabled = "false",
+    energy_required = 2,
+    ingredients = 
+    {
+      {"iron-plate",20},
+      {"engine-unit",6},
+      {"steel-plate",6},
+      {"glass-plate",4},
+      
+      
+      
+    },
+    result = "solar-panel"
+  },
+  {
+    type = "recipe",
+    name = "tank",
+    enabled = "false",
+    energy_required = 2,
+    ingredients = 
+    {
+      {"advanced-circuit",10},
+      {"engine-unit",25},
+      {"steel-plate",40},
+      {"hr-glass",10},
+      
+      
+      
+    },
+    result = "tank"
+  },
+  })
